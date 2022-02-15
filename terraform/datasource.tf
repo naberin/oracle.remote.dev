60517076f1aaac448845ce46e1436a767fb08b75
naberin <aberinnj@gmail.com>
2022-02-15 02:11:16 -0600

    Added datasources and tls_private_key

---
+++ terraform/datasource.tf
@@ -0,0 +1,18 @@
+data "oci_core_images" instanceOCID {
+  compartment_id           = var.compartment_ocid
+  operating_system         = var.instance_os
+  operating_system_version = var.linux_os_version
+  shape                    = var.instance_shape
+
+  filter {
+    name   = "display_name"
+    values = ["^.*Oracle[^G]*$"]
+    regex  = true
+  }
+}
+
+data "oci_identity_availability_domains" ADs {
+
+  compartment_id = var.compartment_ocid
+
+}
